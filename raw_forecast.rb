8e1c6ce0427979810e93e64575989d41c95e51b3
Jason Jordan <jasonjordan@Jasons-MacBook.local>
2016-06-02 16:23:32 -0400

    Working RawForecast object

--- raw_forecast.rb
+++ raw_forecast.rb
@@ -1,7 +1,12 @@
-class Forecast
-  def initialize celsius_temp, rain_chance, date_in_s
-    @celsius_temp   = celsius_temp
-    @rain_chance    = rain_chance
-    @date_in_s = date_in_s
+class RawForecast
+
+  attr_reader :date_in_s, :location_zip, :celsius_temp, :fahrenheit_temp, :rain_chance
+
+  def initialize date_in_s:, location_zip:, celsius_temp: nil, fahrenheit_temp: nil, rain_chance: nil
+    @celsius_temp    = celsius_temp
+    @fahrenheit_temp = fahrenheit_temp
+    @rain_chance     = rain_chance
+    @date_in_s       = date_in_s
+    @location_zip    = location_zip
   end
 end
