a71aa3639b7f2c57e47c1081e3183fe3db19770f
Zach H <zdhadgraft@gtcc.edu>
2016-06-02 14:58:39 -0400

    Added skeleton for basic user functions.

---
+++ event_planner.rb
@@ -0,0 +1,16 @@
+# View Events
+
+## Print event date
+
+## Print forecast
+
+# Create New Event
+
+puts "Please provide a name for the event"
+puts "Please enter the data for the event (YYYY-MM-DD): "
+
+# Delete Event
+
+# Main Menu
+
+# Exit
