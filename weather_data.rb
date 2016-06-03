0c322257e6fbb7326200cf2b6c5144b947c5c589
Jason Jordan <jasonjordan@Jasons-MacBook.local>
2016-06-03 13:57:19 -0400

    Initial commit for weather_data

--- weather_data.rb
+++ weather_data.rb
@@ -1,17 +1,20 @@
-require 'httparty'
 require 'pry'
 require 'json'
+require './weather_parser'
 require './raw_forecast'
 
-# example query for 10 day forecast in areacode 27701
-# r = HTTParty.get "http://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/features/forecast10day/q/27701.json"
-sent_raw_forecast = RawForecast.new date_in_s: "1341468000", location_zip: "27701"
 
+class WeatherData
 
-def file_path file_name
-  File.expand_path "../#{file_name}.json", __FILE__
+  attr_reader :request, :time
+
+  def initialize request
+    @request  = request
+    @time     = request.date_of_event
+  end
+
+  # def get_time
+  #   @time = request.date_of_event
+  # end
+
 end
-
-r = JSON.parse(File.read file_path("example_weather_data"))
-
-binding.pry
