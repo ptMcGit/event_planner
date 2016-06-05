1dae70bff6acad1712ba1bffef43d71d5d5148a1
Jason Jordan <jasonjordan@Jasons-MacBook.local>
2016-06-05 17:03:58 -0400

    Fixed parser to acceptrange of time

--- weather_parser.rb
+++ weather_parser.rb
@@ -18,10 +18,9 @@ class WeatherParser
   end
 
   def parse!
-    # raw = JSON.parse(input)
     if input["forecast"]
       input["forecast"]["simpleforecast"]["forecastday"].each do |day|
-        if day["date"]["epoch"] == date_of_event
+        if (day["date"]["epoch"].to_i >= date_of_event.to_i - 43200) && (day["date"]["epoch"].to_i < date_of_event.to_i + 43200)
           @ftemp_day_hilo.push(day["high"]["fahrenheit"])
           @ftemp_day_hilo.push(day["low"]["fahrenheit"])
           @ctemp_day_hilo.push(day["high"]["celsius"])
@@ -31,7 +30,7 @@ class WeatherParser
       end
     elsif input["hourly_forecast"]
       input["hourly_forecast"].each do |hour|
-        if hour["FCTTIME"]["epoch"] == date_of_event
+        if (hour["FCTTIME"]["epoch"].to_i >= date_of_event.to_i - 1800) && (hour["FCTTIME"]["epoch"].to_i < date_of_event.to_i + 1800)
           @ftemp_event_hour = hour["temp"]["english"]
           @ctemp_event_hour = hour["temp"]["metric"]
           @rain_chance = hour["pop"].to_s
