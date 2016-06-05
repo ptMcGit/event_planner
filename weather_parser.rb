d1be0a4e116f0b186a5246f2a0cb5f6c2d1a8afd
Jason Jordan <jasonjordan@Jasons-MacBook.local>
2016-06-05 19:08:32 -0400

    Refactored each loop into a select

--- weather_parser.rb
+++ weather_parser.rb
@@ -17,25 +17,43 @@ class WeatherParser
     @ftemp_event_hour = nil
   end
 
+  # def parse!
+  #   if input["forecast"]
+  #     input["forecast"]["simpleforecast"]["forecastday"].each do |day|
+  #       if (day["date"]["epoch"].to_i >= date_of_event.to_i - 43200) && (day["date"]["epoch"].to_i < date_of_event.to_i + 43200)
+  #         @ftemp_day_hilo = [(day["high"]["fahrenheit"])]
+  #         @ftemp_day_hilo.push(day["low"]["fahrenheit"])
+  #         @ctemp_day_hilo = [(day["high"]["celsius"])]
+  #         @ctemp_day_hilo.push(day["low"]["celsius"])
+  #         @rain_chance = day["pop"].to_s
+  #       end
+  #     end
+  #   elsif input["hourly_forecast"]
+  #     input["hourly_forecast"].each do |hour|
+  #       if (hour["FCTTIME"]["epoch"].to_i >= date_of_event.to_i - 1800) && (hour["FCTTIME"]["epoch"].to_i < date_of_event.to_i + 1800)
+  #         @ftemp_event_hour = hour["temp"]["english"]
+  #         @ctemp_event_hour = hour["temp"]["metric"]
+  #         @rain_chance = hour["pop"].to_s
+  #       end
+  #     end
+  #   else
+  #     #do nothing
+  #   end
+  # end
+
   def parse!
     if input["forecast"]
-      input["forecast"]["simpleforecast"]["forecastday"].each do |day|
-        if (day["date"]["epoch"].to_i >= date_of_event.to_i - 43200) && (day["date"]["epoch"].to_i < date_of_event.to_i + 43200)
-          @ftemp_day_hilo = [(day["high"]["fahrenheit"])]
-          @ftemp_day_hilo.push(day["low"]["fahrenheit"])
-          @ctemp_day_hilo = [(day["high"]["celsius"])]
-          @ctemp_day_hilo.push(day["low"]["celsius"])
-          @rain_chance = day["pop"].to_s
-        end
-      end
+      day = input["forecast"]["simpleforecast"]["forecastday"].select{ |day| (day["date"]["epoch"].to_i >= date_of_event.to_i - 43200) && (day["date"]["epoch"].to_i < date_of_event.to_i + 43200)}
+        @ftemp_day_hilo = [(day.first["high"]["fahrenheit"])]
+        @ftemp_day_hilo.push(day.first["low"]["fahrenheit"])
+        @ctemp_day_hilo = [(day.first["high"]["celsius"])]
+        @ctemp_day_hilo.push(day.first["low"]["celsius"])
+        @rain_chance = day.first["pop"].to_s
     elsif input["hourly_forecast"]
-      input["hourly_forecast"].each do |hour|
-        if (hour["FCTTIME"]["epoch"].to_i >= date_of_event.to_i - 1800) && (hour["FCTTIME"]["epoch"].to_i < date_of_event.to_i + 1800)
-          @ftemp_event_hour = hour["temp"]["english"]
-          @ctemp_event_hour = hour["temp"]["metric"]
-          @rain_chance = hour["pop"].to_s
-        end
-      end
+      hour = input["hourly_forecast"].select{ |hour| (hour["FCTTIME"]["epoch"].to_i >= date_of_event.to_i - 1800) && (hour["FCTTIME"]["epoch"].to_i < date_of_event.to_i + 1800)}
+        @ftemp_event_hour = hour.first["temp"]["english"]
+        @ctemp_event_hour = hour.first["temp"]["metric"]
+        @rain_chance = hour.first["pop"].to_s
     else
       #do nothing
     end
