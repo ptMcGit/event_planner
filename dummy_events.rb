586f63cbef41223ce711a57f47cb80f6b6bf303b
Zach H <zdhadgraft@gtcc.edu>
2016-06-05 20:37:23 -0400

    Added additional event test cases; random event times now configurable.

--- dummy_events.rb
+++ dummy_events.rb
@@ -1,9 +1,8 @@
 # test events
 
-def valid_future_date
+def valid_future_date max_days_out=12, min_secs_out=1800
   current_block = Time.now - (Time.now.tv_sec % 1800)
-  max_days_out = 12
-  future_blocks = (1800..(86400 * max_days_out)).step(1800).to_a
+  future_blocks = (min_secs_out..(86400 * max_days_out)).step(1800).to_a
 
   Time.at(
     current_block +
@@ -34,3 +33,19 @@ Expired_date_event =
       zip_code: "27401",
       duration_sec: "1800"
     }
+
+In_one_day_event =
+  {
+    title: "My event that happens less than day",
+    date: valid_future_date(1, 3600),
+    zip_code: "64575",
+    duration_sec: "5400"
+  }
+
+In_two_days_event =
+  {
+    title: "My 1 < event < 3 days.",
+    date: valid_future_date(3, 3600 * 24),
+    zip_code: "64575",
+    duration_sec: "5400"
+  }
