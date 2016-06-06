f769a6441cce9883cd445522edb63e235f1113a6
Zach H <zdhadgraft@gtcc.edu>
2016-06-05 20:38:21 -0400

    Improved is_stale? function; added test to check handling of multiple forecasts...
    - Added test to make sure forecasts are updated

--- forecast_tests.rb
+++ forecast_tests.rb
@@ -26,7 +26,6 @@ class EventTestCase < Event
   def populate_dummy_forecasts
     dummy_forecasts = []
     (0..@duration_sec.to_i).step(3600).to_a.each do |f|
-      #dummy_forecasts.push(get_dummy_forecast((@date.to_i + f).to_s, @forecast_type))
       dummy_forecasts.push(get_dummy_forecast((f + @date.to_i).to_s, @zip_code, @forecast_filled_date, @forecast_type))
     end
     @forecasts = dummy_forecasts
@@ -36,18 +35,6 @@ class EventTestCase < Event
     get_dummy_forecast(forecast_date.to_s, zip_code)
   end
 
-  def populate_forecasts
-    new_forecasts = {}
-    (0..@duration_sec.to_i).step(3600).to_a.each do |f|
-      old_f = find_available_forecast((f + @date.to_i).to_s)
-      if is_stale?(old_f) || (not (old_f))
-      forecasts.push get_forecast((f + @date.to_i).to_s)
-      else
-        new_forecasts.push old_f
-      end
-      @forecasts = new_forecasts
-    end
-  end
 
 end
 
@@ -55,8 +42,12 @@ class EventTestCaseTests < Minitest::Test
    include Rack::Test::Methods
    require './dummy_events'
 
-   def stale_date
-     Time.now.tv_sec - (3600..(3600 * 24 * (rand(0..50))))
+   def stale_by_days days=rand(50)
+     Time.now.tv_sec - days * 24 * 3600
+   end
+
+   def stale_by_hours hours=1
+     Time.now.tv_sec - hours * 3600
    end
 
    def current_date
@@ -95,8 +86,6 @@ class EventTestCaseTests < Minitest::Test
      )
    end
 
-   focus
-
    def test_can_create_event_test_case
      d = current_date
      x = EventTestCase.new **(Rainy_birthday), forecast_type: "infinite forecast", forecast_filled_date: d
@@ -104,12 +93,34 @@ class EventTestCaseTests < Minitest::Test
      assert_equal [], x.forecasts[0]["ctemp_day_hilo"]
      assert_equal [], x.forecasts[0]["ftemp_day_hilo"]
      assert_equal nil, x.forecasts[0]["rain_chance"]
-     binding.pry
+
      assert_equal Rainy_birthday[:date], x.forecasts[0]["date_of_event"]
      assert_equal Rainy_birthday[:zip_code], x.forecasts[0]["location_zip"]
      assert_equal nil, x.forecasts[0]["ctemp_event_hour"]
      assert_equal nil, x.forecasts[0]["ftemp_event_hour"]
-     binding.pry
    end
 
+   def test_can_create_test_case_with_multiple_forecasts
+     d = current_date
+     Socal_reggae[:date] = valid_future_date 1
+     x = EventTestCase.new **(Socal_reggae), forecast_type: "three forecast", forecast_filled_date: d
+     assert_equal 2, x.forecasts.count
+     refute_empty x.forecasts[0]
+     refute_empty x.forecasts[1]
+   end
+
+   focus
+
+
+
+   def test_stale_forecasts_updated
+     d = stale_by_days 10
+     x = EventTestCase.new **(In_one_day_event), forecast_type: "three forecast", forecast_filled_date: d
+
+     x.populate_forecasts
+     assert_equal ["Updated values"], x.forecasts[0]["ctemp_day_hilo"]
+     assert_equal In_one_day_event[:date], x.forecasts[0]["date_of_event"]
+     assert x.forecasts[0]["forecast_filled_date"] > d
+
+   end
 end
